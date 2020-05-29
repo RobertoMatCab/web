29b9e0fbd35197246cca3c33401d1c59bc3ff039
RobertoMatCab <36885528+RobertoMatCab@users.noreply.github.com>
2020-05-29 17:48:50 +0200

    first commit

---
+++ dist/precache-manifest.c0810e49cf85c6838e77987f4e552937.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a03d55ed1bd8ded772",
+    "url": "/css/app.cbd0c9e4.css"
+  },
+  {
+    "revision": "9c48f2d0a2cb6f58ec45",
+    "url": "/css/chunk-vendors.abfa80e4.css"
+  },
+  {
+    "revision": "a107bd19d31060797b25d2d005f36d87",
+    "url": "/firebase-messaging-sw.js"
+  },
+  {
+    "revision": "fd7270bd488346d6b0119f33a0100cc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08a03d55ed1bd8ded772",
+    "url": "/js/app.8c8c2815.js"
+  },
+  {
+    "revision": "9c48f2d0a2cb6f58ec45",
+    "url": "/js/chunk-vendors.9e4a239e.js"
+  },
+  {
+    "revision": "ac26741c5709aa6338487085bec1cf5f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
